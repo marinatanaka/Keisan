d55ee3062c43cd13af68ff92771c8cb9bd986db6
marina tanaka <tanakamarina094@gmail.com>
2020-05-08 16:21:51 +0900

    2回目のコミット

--- app/src/main/java/jp/techacademy/marina/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/marina/calcapp/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+import android.provider.Settings.NameValueTable.VALUE
+import android.renderscript.Sampler
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlin.reflect.KParameter
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -22,13 +24,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         override fun onClick(v: View) {
             val intent = Intent(this, SecondcalcActivity:: class.java)
             intent.putExtra("VALUE",0)
-            var num1:Double = editText1.text.toDouble
-            var num2:Double = editText2.text.toDouble
+            var num1:Double = editText1.text.toString().toDouble()
+            var num2:Double = editText2.text.toString().toDouble()
 
             try {
 
             if(v.id == R.id.button1) {
-                VALUE = "${num1 + num2}"
+                VALUE= "${num1 + num2}"
             }else if (v.id == R.id.button2){
                 VALUE = "${num1 - num2}"
             }else if (v.id == R.id.button3) {
